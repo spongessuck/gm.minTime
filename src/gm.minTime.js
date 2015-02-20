8b5ae3f0831ee3a1bd94b93bf2410f10b67d7ca5
Greg McGee <spongessuck@gmail.com>
2015-02-20 16:17:07 -0500

    Fix bower, bump version

--- src/gm.minTime.js
+++ src/gm.minTime.js
@@ -1,22 +1,24 @@
-angular.module('gm.minTime', [])
-.directive('minTime', function (){ 
-	return {
-		require: 'ngModel',
-		restrict: 'A',
-		link: function(scope, elem, attrs, ctrl) {
-			var minTime;
+(function() {
+	angular.module('gm.minTime', [])
+	.directive('minTime', function (){ 
+		return {
+			require: 'ngModel',
+			restrict: 'A',
+			link: function(scope, elem, attrs, ctrl) {
+				var minTime;
 
-			scope.$watch(attrs.minTime, function(newVal) {
-				minTime = newVal;
-				validate();
-			});
+				scope.$watch(attrs.minTime, function(newVal) {
+					minTime = newVal;
+					validate();
+				});
 
-			scope.$watch(attrs.ngModel, validate);
+				scope.$watch(attrs.ngModel, validate);
 
-			function validate(value) {
-				ctrl.$setValidity('minTime', (minTime < ctrl.$modelValue));
-				return value;
+				function validate(value) {
+					ctrl.$setValidity('minTime', (minTime < ctrl.$modelValue));
+					return value;
+				}
 			}
-		}
-	};
-});
+		};
+	});
+})();
