238ed6d62e39564fc921c40c9c378851c0b94282
Greg McGee <spongessuck@gmail.com>
2015-08-10 12:26:34 -0400

    Made ESLint valid, made to work for string representations of dates.

--- src/gm.minTime.js
+++ src/gm.minTime.js
@@ -1,24 +1,39 @@
-(function() {
+(function(angular) {
+	'use strict';
 	angular.module('gm.minTime', [])
-	.directive('minTime', function (){ 
+	.directive('minTime', function () {
 		return {
 			require: 'ngModel',
 			restrict: 'A',
 			link: function(scope, elem, attrs, ctrl) {
 				var minTime;
 
+				function validate(value) {
+					var modelValue = ctrl.$modelValue;
+
+					if(typeof modelValue == 'string') {
+						var timeValue = Date.parse(modelValue);
+						if(!isNaN(timeValue))
+							modelValue = timeValue;
+					}
+
+					ctrl.$setValidity('minTime', minTime < modelValue);
+					return value;
+				}
+
 				scope.$watch(attrs.minTime, function(newVal) {
+					if(typeof newVal == 'string') {
+						var timeValue = Date.parse(newVal);
+						if(!isNaN(timeValue))
+							newVal = timeValue;
+					}
+
 					minTime = newVal;
 					validate();
 				});
 
 				scope.$watch(attrs.ngModel, validate);
-
-				function validate(value) {
-					ctrl.$setValidity('minTime', (minTime < ctrl.$modelValue));
-					return value;
-				}
 			}
 		};
 	});
-})();
+})(window.angular);
